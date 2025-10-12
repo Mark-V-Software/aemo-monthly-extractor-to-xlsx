bf71f59bde0c06beaf4646a7e0aeb17b278c5024
Mark V <mark.vizzhachiy@gmail.com>
2025-10-12 19:18:26 +0700

    Delete

--- aemo_monthly_extractor_to_xlsx[win10-amd64].ps1
+++
@@ -1,3 +0,0 @@
-# === Windows 10 or newer ===
-& "uv-x86_64-pc-windows-msvc\uv.exe" run aemo_monthly_extractor_to_xlsx.py
-# How To Start: powershell -ExecutionPolicy Bypass -File aemo_monthly_extractor_to_xlsx[win10-amd64].ps1
